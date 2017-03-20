ec323d0763623849594e29399b64a5b8cf78770d
danielszentesi <jonidani96@gmail.com>
2017-03-20 09:01:48 -0700

    half done tests2

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,13 +3,18 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.user.TrainUserImpl;
+
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
     @Before
     public void before() {
-        // TODO Add initializations
+        TrainUserImpl mockTU = mock(TrainUserImpl.class);
+        
     }
 
     @Test
